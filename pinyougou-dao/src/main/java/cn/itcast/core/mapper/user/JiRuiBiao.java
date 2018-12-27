f77e19173ff5f99820b9ffe29bf00b80dc37601e
ruigege <1826078372@qq.com>
2018-12-27 18:10:57 +0800

    ceshi

--- pinyougou-dao/src/main/java/cn/itcast/core/mapper/user/JiRuiBiao.java
+++ pinyougou-dao/src/main/java/cn/itcast/core/mapper/user/JiRuiBiao.java
@@ -2,6 +2,6 @@ package cn.itcast.core.mapper.user;
 
 public class JiRuiBiao {
     public static void main(String[] args) {
-        System.out.println("hello World1");
+        System.out.println("hello World11");
     }
 }
